ab23d0a487606bee27bf78f9dd1db897eb9b9dba
eradoria <eradoria@gmail.com>
2022-08-18 21:52:31 -0500

    1st

--- src/Router.js
+++ src/Router.js
@@ -11,7 +11,18 @@ Remember to export the component at the end
 import React from 'react'
 import { Routes, Route } from 'react-router'
 // Write component imports here //
-
-
+import About from "./components/About"
+import Home from "./components/Home"
 
 // Start Router function here //
+function Router() {
+    return (
+        <Routes>
+            <Route path="/" component={<Home/>} />
+            <Route path="/about" component={<About/>} />
+        </Routes>
+    );
+}
+
+export default Router;
+
